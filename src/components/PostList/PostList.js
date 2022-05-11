e2d545cb112e8c24a3373b91fe8f3ac5b4455154
eldar <eldar158@gmail.com>
2022-05-11 15:22:37 +0300

    added list animation using react-flip-move

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+import FlipMove from 'react-flip-move';
+
 import './PostList.css';
 
 import PostBlock from './PostBlock/PostBlock'
@@ -7,35 +9,39 @@ import PostBlock from './PostBlock/PostBlock'
 
 function PostList(props) {
   const {posts, displayCount} = props
+
   const [first, setFirst] = useState(0)
+  const [displayed, setDisplayed] = useState(posts.slice(0, displayCount))
 
   //interval between displayed posts
   useEffect(() => {
     const interval = setInterval(() => {
-      const next = (first + 1) % posts.length
-      setFirst(next)
-    }, 3000);
-    return () => clearInterval(interval);
-  }, [posts, first]);
+      const newFirst = (first + 1) % posts.length
+      setFirst(newFirst)
 
-  //choose which posts to display
-  const displayed = []
-  for (let i = first; i < first + displayCount; i++) {
-    displayed.push(posts[i % posts.length])
-  }
+      const newDisplayed = displayed.slice(1, displayed.length)
+      newDisplayed.push(posts[(newFirst + displayCount - 1) % posts.length])
+      setDisplayed(newDisplayed)
+    }, 2000)
+
+    return () => clearInterval(interval);
+  }, [posts, first, displayed, displayCount]);
+  
 
   return (
     <div className="PostList">
-      {
-        displayed.map((post, i) => 
-          <PostBlock key={i}
-            id={post.id}
-            title={post.title}
-            description={post.description}
-            pictureId={post.pictureId}
-          ></PostBlock>
-        )
-      }
+      <FlipMove>
+        {displayed.map((post) => 
+          <div key={post.id}>
+            <PostBlock
+              id={post.id}
+              title={post.title}
+              description={post.description}
+              pictureId={post.pictureId}
+            ></PostBlock>
+          </div>
+        )}
+      </FlipMove>
     </div>
   );
 }
